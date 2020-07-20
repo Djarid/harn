dc7e801eba6cc47907079a313fcb83bed54b1aa2
Djarid <djarid997@gmail.com>
2020-07-20 12:11:14 +0100

    Provide working spell list

---
+++ spellbook/src/components/SpellBook.vue
@@ -0,0 +1,42 @@
+<template>
+    <div id="spellbook">
+        <h2>The Grimoires of {{ this.charName }}</h2>
+        <ul>
+            <li>
+                <input class="header" type="text" v-for="(v,k) in spells[0]" :key=k :value="k">
+            </li>
+            <li v-for="(spell, index) in spellList" :key=index>                
+                <input type="text" v-for="(v,k) in spell" :key=k v-model="spells[index][k]">
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import grimoire from '../assets/spells.json'
+export default {
+    name: "spellbook",
+    props: { 
+        charName: String 
+    },
+    data: function() {
+        return {
+            spells: grimoire.spells,
+            legend: grimoire.legend,
+            convocations: grimoire.convocations
+        }
+    },
+   computed: {
+           spellList: function() {
+                var filteredList = this.spells
+                return filteredList
+           }
+   }}
+</script>
+
+
+<style lang="scss" scoped>
+.header {
+    font-weight: bold;
+}
+</style>
---
+++ spellbook/src/views/Spells.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="spellbook">
+    <SpellBook charName="Nuada Halirien"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import SpellBook from '@/components/SpellBook.vue'
+
+export default {
+  name: 'Spells',
+  components: {
+    SpellBook
+  }
+}
+</script>
